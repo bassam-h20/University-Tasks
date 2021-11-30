f490a3c6cd8b66e468ad7e5aeee8fcb8cd8bed93
BB644 <95085280+bassam-h20@users.noreply.github.com>
2021-11-30 17:46:03 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cmath>
+
+
+float CompoundInterest (int p, float r, int t)
+ {
+
+  float A;
+  float euler = 2.71828182845904523536;
+  return A = (p) * (pow(euler, ((r/100)*t)));
+ }
+
+
+
+int main() 
+{
+
+int originalAmount, years;
+float e, interestRate;
+
+printf("\n\t\t\tAnnual Compound Interest Calculator\n\n");
+
+printf("\nEnter original amount deposited: \n");
+scanf("%d",&originalAmount);
+
+printf("\nEnter amount of years: \n");
+scanf("%d",&years);
+
+printf("\nEnter interest rate percentage: \n");
+scanf("%f",&interestRate);
+
+float res = CompoundInterest(originalAmount, interestRate, years);
+
+printf("\n\n\nFinal value of account: %f",res);
+
+
+} 
